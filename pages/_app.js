06e5daff7243f7e474770cc3e34dcb17f5b2129d
Renan Paixão <renanlemos21@gmail.com>
2021-07-18 13:49:10 -0300

    feat(main): criação e estilização do component main, além de resets globais

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,44 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color:  #D9E6F6;
   }
-`
+  #__next{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  img{
+    height: auto;
+    max-width: 100%;
+    display: block;
+
+  }
+`;
 
 const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+	colors: {
+		primary: '#0070f3',
+	},
+};
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+	return (
+		<>
+			<GlobalStyle />
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
