9d4c631d7d3c28212e5625bc0e1f7829c0f3cdf7
Renan Paixão <renanlemos21@gmail.com>
2021-07-18 13:52:46 -0300

    feat(app): inicialização da estrutura da página
    Criação da Sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
-import styled from 'styled-components'
+import Box from '../src/components/Box';
+import Main from '../src/components/Main';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
+function Sidebar({ user }) {
+	return (
+		<div className="profileArea" style={{ gridArea: 'profileArea' }}>
+			<Box>
+				<img src={`https://github.com/${user}.png`} style={{ borderRadius: '8px;' }}></img>
+			</Box>
+		</div>
+	);
+}
 export default function Home() {
-  return <Title>My page</Title>
+	const githubUser = 'RenanPaixao';
+	return (
+		<Main>
+			<Sidebar user={githubUser}></Sidebar>
+			<div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+				<Box>Bem vindo(a)</Box>
+			</div>
+			<div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+				<Box>Friends</Box>
+			</div>
+		</Main>
+	);
 }
